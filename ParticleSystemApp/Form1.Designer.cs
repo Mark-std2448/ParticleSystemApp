da3fbe5533fede27587aa3060e4a798629441f19
Mark-std2448 <markshishishkin@mail.ru>
2026-05-08 10:43:16 +0800

    Создана первоначальаня версия графического интерфейса

--- ParticleSystemApp/Form1.Designer.cs
+++ ParticleSystemApp/Form1.Designer.cs
@@ -29,12 +29,58 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tbRadius = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRadius)).BeginInit();
+            this.SuspendLayout();
+            
+            
+            
+            this.picDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picDisplay.Location = new System.Drawing.Point(0, 0);
+            this.picDisplay.Name = "picDisplay";
+            this.picDisplay.Size = new System.Drawing.Size(800, 450);
+            this.picDisplay.TabIndex = 0;
+            this.picDisplay.TabStop = false;
+            
+            
+            
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            
+            
+            
+            this.tbRadius.Location = new System.Drawing.Point(339, 111);
+            this.tbRadius.Maximum = 200;
+            this.tbRadius.Minimum = 10;
+            this.tbRadius.Name = "tbRadius";
+            this.tbRadius.Size = new System.Drawing.Size(104, 69);
+            this.tbRadius.TabIndex = 1;
+            this.tbRadius.Value = 10;
+            
+            
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbRadius);
+            this.Controls.Add(this.picDisplay);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRadius)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar tbRadius;
     }
 }
 
